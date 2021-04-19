247bac60c5002f5e4415b2fe9a7fcc88bec248b7
alisa09200 <alisa_yamazaki_1119@yahoo.co.jp>
2021-04-19 09:42:11 +0900

    テーブル作成

--- db/migrate/20210419003120_create_participants.rb
+++ db/migrate/20210419003120_create_participants.rb
@@ -1,6 +1,8 @@
 class CreateParticipants < ActiveRecord::Migration[6.0]
   def change
     create_table :participants do |t|
+      t.string :name
+      t.integer :reservation_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_18_091250) do
+ActiveRecord::Schema.define(version: 2021_04_19_003120) do
+
+  create_table "participants", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.integer "reservation_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "reservations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
