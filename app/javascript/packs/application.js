cf2fb15d5ce3981d6b01670be0d25f4de63f5bd6
alisa09200 <alisa_yamazaki_1119@yahoo.co.jp>
2021-04-22 08:06:09 +0900

    ファイルの読み込み

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
+require('vendor/main.js')
+require('vendor/jquery-1.10.2.min.js')
+require('vendor/jquery-ui-1.10.3.custom.min.js')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
