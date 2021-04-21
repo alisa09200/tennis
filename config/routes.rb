d629e48143016452c406a4faa27f64f2ab659f42
alisa09200 <alisa_yamazaki_1119@yahoo.co.jp>
2021-04-22 08:14:38 +0900

    ルーティング訂正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'main/index'
   root 'main#index'
   devise_for :users
-  root 'reservations#index'
   resources :reservations do
     resources :participants, only: :create
   end
