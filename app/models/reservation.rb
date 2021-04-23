3ae46fb462e8db5b1efd7d8e497790034fe9e88e
alisa09200 <alisa_yamazaki_1119@yahoo.co.jp>
2021-04-23 11:04:01 +0900

    アソシエーション記載

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,6 +2,7 @@ class Reservation < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   has_many :participants
   belongs_to :city
+  has_many :comments
 
   validates :city_id, numericality: { other_than: 1 }
 end
