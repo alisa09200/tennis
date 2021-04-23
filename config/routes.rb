e63a67e78937d9ad79b77263738e51f0a2cb862e
alisa09200 <alisa_yamazaki_1119@yahoo.co.jp>
2021-04-23 11:06:03 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :reservations do
     resources :participants, only: :create
+    resources :comments, only: :create
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
