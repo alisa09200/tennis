07bc9345a8bc9404f304e225b61d3bd3be2be415
alisa09200 <alisa_yamazaki_1119@yahoo.co.jp>
2021-04-23 12:11:18 +0900

    reservationsテーブルにuser_idカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_23_025709) do
+ActiveRecord::Schema.define(version: 2021_04_23_030955) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_04_23_025709) do
     t.string "level"
     t.text "area"
     t.integer "city_id"
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
