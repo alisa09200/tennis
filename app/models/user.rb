bfe161425418b9b4fc914bcf8cbf02236fa1e95a
alisa09200 <alisa_yamazaki_1119@yahoo.co.jp>
2021-05-12 19:48:50 +0900

    クラスメソッドguestを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
   has_many :comments
   has_many :reservations
   has_many :participants
+  def self.guest
+    find_or_create_by(email: "test@com") do |user|
+      user.password = Rails.application.secrets.test_account_pass
+    end
+  end
 end
